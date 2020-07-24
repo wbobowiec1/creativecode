32f72e7b397b3d63ed11a7d003aa4c55e82e73d7
wbobowiec1 <67722237+wbobowiec1@users.noreply.github.com>
2020-07-24 10:00:28 -0700

    Update and rename concentric-clock.js to clock.js

---
+++ clock/clock.js
@@ -0,0 +1,68 @@
+var col1 = {
+  r1: 0,
+  g1: 0,
+  b1: 225
+};
+
+
+function setup() {
+  createCanvas(1000, 1000);
+  angleMode(DEGREES);
+  extraCanvas = createGraphics(1000, 1000);
+  extraCanvas.clear();
+}
+
+function draw() {
+  background(50, 0, 50);
+  let hours = hour();
+  let mins = minute();
+  let secs = second();
+  console.log(hours, mins, secs);
+  translate(width / 2, height / 2);
+  rotate(-90);
+
+  strokeWeight(30);
+  stroke(0, 0, 250);
+  noFill();
+  let secsDegree = map(secs, 0, 60, 0, 360);
+  arc(0, 0, 500, 500, 0, secsDegree);
+
+  push();
+  rotate(secsDegree);
+  line(0, 0, 300, 0);
+  pop();
+
+  strokeWeight(25);
+  stroke(0, 100, 250);
+  noFill();
+  let minsDegree = map(mins, 0, 60, 0, 360);
+  arc(0, 0, 400, 400, 0, minsDegree);
+
+  push();
+  rotate(minsDegree);
+  line(0, 0, 250, 0);
+  pop();
+
+  strokeWeight(23);
+  stroke(0, 200, 200);
+  noFill();
+  let hoursDegree = map(hours, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, hoursDegree);
+
+  push();
+  rotate(hoursDegree);
+  line(0, 0, 200, 0);
+  pop();
+
+  col1.r1 = random(100, 200);
+  col1.g1 = random(50, 200);
+  col1.b1 = random(0, 225);
+
+
+  extraCanvas.fill(col1.r1, col1.g1, col1.b1);
+  extraCanvas.noStroke();
+  extraCanvas.rect(480, 470, 50, 50, 100);
+
+  image(extraCanvas, -500, -500);
+
+}
